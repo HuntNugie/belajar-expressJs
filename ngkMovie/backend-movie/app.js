277a21a989e848dcaa94f2f5a8b252466f7a0f39
Nugie kurniawan <nugiebinhar@gmail.com>
2025-09-27 01:19:04 +0700

    projek/berhasil membuat backend default yang dapat di pakai oleh front end

---
+++ ngkMovie/backend-movie/app.js
@@ -0,0 +1,29 @@
+import express from "express"
+import cors from "cors"
+import axios from "axios"
+const API = "http://www.omdbapi.com/?apikey=bb6e65b5"
+const app = express()
+
+app.use(cors())
+app.get("/",(req,res)=>{
+    res.header("Allow-Access-Control-origin","*")
+    res.json({message:"Ini adalah backend dari ngkMovie",author:"Nugie kurniawan"})
+})
+
+app.get("/API/movie",async(req,res)=>{
+    try{
+        const response = await axios.get(API+"&s=ultraman")
+        const status = {status:"true",result:"berhasil ambil film"}
+        res.json({
+            res:status,
+            data:response.data.Search
+        })
+    } catch(err){
+        res.json({status:"false",result:"gagal mengambil film"})
+    }
+})
+
+
+app.listen(3000,()=>{
+    console.log("Express berjalan di http://localhost:3000")
+})
