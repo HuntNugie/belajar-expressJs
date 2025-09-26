861249c47ebc68e2b322dec8185955cb9ea6b137
Nugie kurniawan <nugiebinhar@gmail.com>
2025-09-26 23:36:41 +0700

    fundamental/middleware

--- fundamental/app.js
+++ fundamental/app.js
@@ -1,10 +1,17 @@
 import express from "express";
-
+1
 const app = express()
 
+
+// ini untuk request get
 app.get("/",(req,res)=>{
     res.send("Berhasil anjing kontol bangsat")
 })
+// ini untuk middleware
+app.use("/",(req,res)=>{
+    res.send("Ini dari middleware")
+})
+
 
 // buat server
 app.listen(3000,()=>{
