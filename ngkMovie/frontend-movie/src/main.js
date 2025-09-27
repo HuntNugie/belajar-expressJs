972d974c9350ababd95fa944f4fba8623a42dbe7
Nugie kurniawan <nugiebinhar@gmail.com>
2025-09-27 08:50:11 +0700

    projek/fitur searching dan error handler

--- ngkMovie/frontend-movie/src/main.js
+++ ngkMovie/frontend-movie/src/main.js
@@ -1,16 +1,24 @@
 import './style.css'
-
-const API = "http://localhost:3000/API/movie"
+import debounce from "debounce"
+const API = "http://localhost:3000"
 const mains = document.getElementById("mainContent")
+const searching = document.getElementById("search")
 
 const getData = async function(link){
-  const response = await fetch(link)
-  const parseJSON = await response.json()
-  const data = parseJSON.data
-  return data
+  try {
+    const response = await fetch(link)
+    const parseJSON = await response.json()
+    if(parseJSON.status === "false"){
+        throw new Error(parseJSON.result)
+    }
+    const data = parseJSON.data
+    return data
+  } catch (error) {
+    throw new Error(error)
+  }
 }
 
-const renderUI = function(response){
+const renderUI = function(response=[]){
   const component = response.map((el)=>{
     return ` <div class="bg-gray-800 rounded-2xl shadow-lg overflow-hidden hover:scale-105 transition transform duration-300">
         <img src="${el.Poster}" alt="Movie Poster" class="w-full h-56 object-cover" >
@@ -21,8 +29,30 @@ const renderUI = function(response){
         </div>
       </div>`
   }).join("")
+
   mains.innerHTML = component
   cekGambar()
+}
+
+// untuk defauults
+const defaults = async (link=API)=>{
+  try {
+  const response = await getData(link+"/API/movie")
+  renderUI(response)
+  } catch (error) {
+  mains.innerHTML = `<h1>${error.message}</h1>`  
+  }
+}
+
+// untuk searching
+const searchings = async(event)=>{
+  try {
+  const hasil = event.target.value
+  const response = await getData(`${API}/API/movie?search=${hasil}`)
+  renderUI(response)
+  } catch (error) {
+  mains.innerHTML = `<h1>${error}</h1>`
+  }
 }
 
 const cekGambar = function(){
@@ -33,9 +63,11 @@ const cekGambar = function(){
     }
   })
 }
+searching.addEventListener("input",debounce(searchings,1000))
 
 window.onload = async(event)=>{
-  const defaults = await getData(API)
-  renderUI(defaults)
+  if(history.state === null){
+    defaults()
+  }
 }
 
