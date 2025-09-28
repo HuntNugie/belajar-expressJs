baa3059cdd2bd9ea45e692dec626341c2e3d6b0e
Nugie kurniawan <nugiebinhar@gmail.com>
2025-09-28 15:00:58 +0700

    belajar render view dan templating engidari ejs

---
+++ renderView/app.js
@@ -0,0 +1,12 @@
+import express from "express"
+
+const app = express()
+
+app.set("view engine","ejs")
+app.get("/",(req,res)=>{
+    res.render("index")
+})
+
+app.listen(3000,()=>{
+    console.log("berjalan di http://localhost:3000")
+})
