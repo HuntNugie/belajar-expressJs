7e332651327ba062aae149149da3d8dee57ec2ab
Nugie kurniawan <nugiebinhar@gmail.com>
2025-09-28 15:35:43 +0700

    menggunakan express ejs layout

--- renderView/app.js
+++ renderView/app.js
@@ -1,10 +1,18 @@
 import express from "express"
-
+import expressEjsLayouts from "express-ejs-layouts"
 const app = express()
 
 app.set("view engine","ejs")
+app.use(expressEjsLayouts)
+
 app.get("/",(req,res)=>{
-    res.render("index",{nama:"aleksander kurniawan"})
+    res.render("index",{nama:"aleksander kurniawan",layout:"layouts/main"})
+})
+app.get("/about",(req,res)=>{
+    res.render("about",{nama:"aleksander kurniawan",layout:"layouts/main"})
+})
+app.get("/contact",(req,res)=>{
+    res.render("contact",{nama:"aleksander kurniawan",layout:"layouts/main"})
 })
 
 app.listen(3000,()=>{
