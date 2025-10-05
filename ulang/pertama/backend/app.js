3f4cf862461e9479f6b1fe4967942f78548368b5
Nugie kurniawan <nugiebinhar@gmail.com>
2025-10-05 22:41:50 +0700

    tes arsitektur

--- ulang/pertama/backend/app.js
+++ ulang/pertama/backend/app.js
@@ -1,8 +1,24 @@
 import express from "express"
 import cors from "cors"
+import Routes from "./Routes/indexRoutes.js"
+import dotenv from "dotenv"
 
+// ini untuk dotenv agar bisa memabca env
+dotenv.config()
 // setup express
 const app = express()
+const port = process.env.PORT 
+
+
 
 app.use(cors())
 
+app.use("/",Routes.homeRoute)
+
+// setup untuk server
+app.listen(port,(error)=>{
+    if(error){
+        throw new Error(error)
+    }
+    console.log(`berjalan di http://localhost:${port}`)
+})
