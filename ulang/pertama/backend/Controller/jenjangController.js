fd1bb4f000cd05512bac24e9aaa36a7d788b9584
Nugie kurniawan <nugiebinhar@gmail.com>
2025-10-07 00:35:55 +0700

    siap untuk perPage dan page jenjang dinamis

--- ulang/pertama/backend/Controller/jenjangController.js
+++ ulang/pertama/backend/Controller/jenjangController.js
@@ -6,7 +6,9 @@ const api = process.env.API
 
 export const index = async (req,res)=>{
     const jenis = req.params.jenis
-    const response = await fetch(`${api}/sekolah/${jenis}?page=1&perPage=5`)
+    const {perPage,page} = req.query
+
+    const response = await fetch(`${api}/sekolah/${jenis}?page=${page}&perPage=${perPage}`)
     const data = await response.json()
     res.json(data)
 }
