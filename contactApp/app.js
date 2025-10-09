6e037dd50e9fafdd664479e243802623af36e558
Nugie kurniawan <nugiebinhar@gmail.com>
2025-10-09 18:46:46 +0700

    setup express dan ejs untuk contact app

---
+++ contactApp/app.js
@@ -0,0 +1,11 @@
+import express from "express"
+import route from "./Routes/indexRoute.js"
+const app = express()
+
+app.set("view engine","ejs")
+
+app.use("/",route.home);
+
+app.listen(3000,(error)=>{
+    console.log("berjalan di port 3000")
+})
