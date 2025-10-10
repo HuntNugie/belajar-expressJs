c585edb4cb4ca5a4b4f818fde6f8e8ef9f3a5357
Nugie kurniawan <nugiebinhar@gmail.com>
2025-10-10 10:49:57 +0700

    memperbaiki tampilan dari form tambah data

--- contactApp/Controller/ContactController.js
+++ contactApp/Controller/ContactController.js
@@ -1,6 +1,6 @@
 import { loadContact,detailContact } from "../utils/indexUtils.js"
 
-
+// untuk me render halaman daftar contact 
 export const index = (req,res)=>{
     const contacts = loadContact()
     res.render("contact",{
@@ -8,8 +8,13 @@ export const index = (req,res)=>{
     })
 }
 
-
+// untuk me render halaman detail contact
 export const show = (req,res)=>{
     const contact = detailContact(req.params.email)
     res.render("detailContact",{contact})
+}
+
+// untuk me render halaman form tambah contact
+export const create = (req,res)=>{
+    res.render("add-contact")
 }
--- contactApp/Routes/contactRoute.js
+++ contactApp/Routes/contactRoute.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { index,show } from "../Controller/ContactController.js";
+import { index,show,create } from "../Controller/ContactController.js";
 
 const route = Router()
 // untuk seluruh data
 route.get("/",index)
-
+// untuk menampilkan form tambah contact
+route.get("/add",create)
 // untuk detail berdasarkan email
 route.get("/:email",show)
 
